5f0dc4f7a5a294d9b728a77d1f3cf092deb2b6b9
MiguelAngelx <miguelcallacondo2@gmail.com>
2020-10-08 20:44:34 +0000

    Tarea

---
+++ Tarea de Fundamentos de Programacion/Ejercicio3.1.py
@@ -0,0 +1,7 @@
+def determinarsiunapersonapuedevotarMAQC():
+    E=int(input("Ingrese el valor de verdad:"))
+    if E>=18:
+        print ("Si puede votar")
+    else:
+        print("No puede votar")
+determinarsiunapersonapuedevotarMAQC() 
