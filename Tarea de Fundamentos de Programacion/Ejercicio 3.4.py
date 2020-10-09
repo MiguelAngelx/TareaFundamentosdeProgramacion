5bcd61b241d8814b5a953f60983f6092f67cf7ff
MiguelAngelx <miguelcallacondo2@gmail.com>
2020-10-09 19:44:53 +0000

    tarea

---
+++ Tarea de Fundamentos de Programacion/Ejercicio 3.4.py
@@ -0,0 +1,16 @@
+def determinarcobroporusoestacionamientoMAQC():
+ hora =int(input("ingrese el numero de horas :"))
+
+ if hora<=2 :
+    cobro = hora*5*1
+ else :
+    hora>=3 and hora<6 
+    cobro=hora*4*1
+
+    if hora>5 and hora>11 :
+        cobro=hora*3*1*1
+    else :
+         hora>10 
+         cobro=hora*2*1*1*1
+ print("el precio total es:",cobro)
+determinarcobroporusoestacionamientoMAQC()
---
+++ Tarea de Fundamentos de Programacion/ejercicio 3.11.py
@@ -0,0 +1,9 @@
+def determinarbonodetrabajador():
+ antiguedad=int(input("ingrese los años trabajados"))
+ if antiguedad<=5 :
+    bono=antiguedad*100
+    print("tu dinero de bono es",bono)
+ else :
+     bono=1000
+     print("tu bono es:",bono)
+determinarbonodetrabajador()
