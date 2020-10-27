79e8603f15abe1ec5d5a913df8416151ce554dd1
MiguelAngelx <70962244+MiguelAngelx@users.noreply.github.com>
2020-10-27 18:40:22 -0500

    Update 4.1.py

--- Estructuras repetitivas tarea/4.1.py
+++ Estructuras repetitivas tarea/4.1.py
@@ -1,8 +1,8 @@
-def CalSalarioNMVA():
+def CalSalarioMAQC():
     str
 print("inicio")
 salario=1500
 for años in [1,2,3,4,5,6]:
      print(f"La cantidad de años es {años} y tu salario es {salario*100/100}")
      salario=salario*1.1
-CalSalarioNMVA()
+CalSalarioMAQC()
